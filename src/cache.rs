c97731a452c052b70b141ec2922a7bf3bfda2820
Sam <sdavarnia@apple.com>
2022-12-26 21:11:51 -0800

    changes

--- src/cache.rs
+++ src/cache.rs
@@ -3,7 +3,7 @@ use std::{error::Error, time::Duration};
 use aws_sdk_s3::{types::ByteStream, Client};
 use rocksdb::Options;
 
-use crate::store::{DiskStore, LRUStore, S3Store, Store};
+use crate::store::{DiskStore, Key, LRUStore, S3Store, Store, Value};
 
 struct Cache<I, O, C> {
     in_memory_store: Box<I>,
@@ -12,7 +12,7 @@ struct Cache<I, O, C> {
 }
 
 impl<I: Store + Copy, O: Store + Copy, C: Store + Copy> Cache<I, O, C> {
-    fn simple_new(
+    fn simple_new<'a>(
         in_memory_lru_capacity: u64,
         disk_store_ttl: Duration,
         client: Client,
@@ -30,9 +30,9 @@ impl<I: Store + Copy, O: Store + Copy, C: Store + Copy> Cache<I, O, C> {
         }
     }
     async fn get(
-        &self,
+        &'static mut self,
         bucket: &str,
-        key: &Vec<u8>,
+        key: &Key,
         from_cache: Option<bool>,
     ) -> Result<Option<ByteStream>, Box<dyn Error>> {
         let read_from_cache = from_cache.unwrap_or(true);
@@ -44,18 +44,13 @@ impl<I: Store + Copy, O: Store + Copy, C: Store + Copy> Cache<I, O, C> {
         }
     }
 
-    async fn put(
-        &mut self,
-        bucket: &str,
-        key: &Vec<u8>,
-        value: &ByteStream,
-    ) -> Result<(), Box<dyn Error>> {
+    async fn put(&mut self, bucket: &str, key: &Key, value: &Value) -> Result<(), Box<dyn Error>> {
         self.cloud_store.put(bucket, key, value).await?;
         self.on_disk_store.put(bucket, key, value).await?;
         self.in_memory_store.put(bucket, key, value).await
     }
 
-    async fn delete(&mut self, bucket: &str, key: &Vec<u8>) -> Result<(), Box<dyn Error>> {
+    async fn delete(&mut self, bucket: &str, key: &Key) -> Result<(), Box<dyn Error>> {
         self.cloud_store.delete(bucket, key).await?;
         self.on_disk_store.delete(bucket, key).await?;
         self.in_memory_store.delete(bucket, key).await
@@ -64,8 +59,8 @@ impl<I: Store + Copy, O: Store + Copy, C: Store + Copy> Cache<I, O, C> {
     async fn put_in_cache(
         &mut self,
         bucket: &str,
-        key: &Vec<u8>,
-        value: &ByteStream,
+        key: &Key,
+        value: &Value,
     ) -> Result<(), Box<dyn Error>> {
         let b = self.in_memory_store.put(bucket, key, value).await?;
         self.on_disk_store.put(bucket, key, value).await
