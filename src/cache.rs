d4a9659d8f4b1cc3fcfe9db7b47adad925e6d12f
Sam <sdavarnia@apple.com>
2022-12-26 22:05:13 -0800

    refactor

--- src/cache.rs
+++ src/cache.rs
@@ -6,9 +6,9 @@ use rocksdb::Options;
 use crate::store::{DiskStore, Key, LRUStore, S3Store, Store, Value};
 
 struct Cache<I, O, C> {
-    in_memory_store: Box<I>,
-    on_disk_store: Box<O>,
-    cloud_store: Box<C>,
+    in_memory_store: I,
+    on_disk_store: O,
+    cloud_store: C,
 }
 
 impl<I: Store + Copy, O: Store + Copy, C: Store + Copy> Cache<I, O, C> {
@@ -18,10 +18,9 @@ impl<I: Store + Copy, O: Store + Copy, C: Store + Copy> Cache<I, O, C> {
         client: Client,
         throttle: Duration,
     ) -> Cache<LRUStore, DiskStore, S3Store> {
-        let mut in_memory_store = Box::new(LRUStore::new(in_memory_lru_capacity));
-        let mut on_disk_store =
-            Box::new(DiskStore::new(&Options::default(), disk_store_ttl, "./db"));
-        let mut cloud_store = Box::new(S3Store { client, throttle });
+        let mut in_memory_store = LRUStore::new(in_memory_lru_capacity);
+        let mut on_disk_store = DiskStore::new(&Options::default(), disk_store_ttl, "./db");
+        let mut cloud_store = S3Store { client, throttle };
 
         Cache {
             in_memory_store,
@@ -39,38 +38,30 @@ impl<I: Store + Copy, O: Store + Copy, C: Store + Copy> Cache<I, O, C> {
 
         if read_from_cache {
             // Check in-memory store first
-            match self.in_memory_store.get(bucket, key).await {
-                Ok(Some(data)) => return Ok(Some(data)),
-                Ok(None) => {}
-                Err(e) => return Err(e),
+            if let Some(data) = self.in_memory_store.get(bucket, key).await? {
+                return Ok(Some(data));
             }
 
             // Check on-disk store next
-            match self.on_disk_store.get(bucket, key).await {
-                Ok(Some(data)) => {
-                    // Store data in in-memory store before returning it
-                    self.in_memory_store.put(bucket, key, &data).await;
-                    return Ok(Some(data));
-                }
-                Ok(None) => {}
-                Err(e) => return Err(e),
+            if let Some(data) = self.on_disk_store.get(bucket, key).await? {
+                // Store data in in-memory store before returning it
+                self.in_memory_store.put(bucket, key, &data).await?;
+                return Ok(Some(data));
             }
 
             // Check cloud store if data is not found in cache
-            match self.cloud_store.get(bucket, key).await {
-                Ok(Some(data)) => {
-                    // Store data in in-memory and on-disk stores before returning it
-                    self.in_memory_store.put(bucket, key, &data).await?;
-                    self.on_disk_store.put(bucket, key, &data).await?;
-                    return Ok(Some(data));
-                }
-                Ok(None) => return Ok(None),
-                Err(e) => return Err(e),
+            if let Some(data) = self.cloud_store.get(bucket, key).await? {
+                // Store data in in-memory and on-disk stores before returning it
+                self.in_memory_store.put(bucket, key, &data).await?;
+                self.on_disk_store.put(bucket, key, &data).await?;
+                return Ok(Some(data));
             }
         } else {
             // Return data from cloud store without checking cache
-            self.cloud_store.get(bucket, key).await
+            return self.cloud_store.get(bucket, key).await;
         }
+
+        Ok(None)
     }
 
     async fn put(&mut self, bucket: &str, key: &Key, value: &Value) -> Result<(), Box<dyn Error>> {
@@ -83,15 +74,5 @@ impl<I: Store + Copy, O: Store + Copy, C: Store + Copy> Cache<I, O, C> {
         self.cloud_store.delete(bucket, key).await?;
         self.on_disk_store.delete(bucket, key).await?;
         self.in_memory_store.delete(bucket, key).await
-    }
-
-    async fn put_in_cache(
-        &mut self,
-        bucket: &str,
-        key: &Key,
-        value: &Value,
-    ) -> Result<(), Box<dyn Error>> {
-        let b = self.in_memory_store.put(bucket, key, value).await?;
-        self.on_disk_store.put(bucket, key, value).await
     }
 }
