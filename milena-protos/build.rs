bedf823d49a62de091742abbd1eb84a8c1d4d9e2
Sam <sdavarnia@apple.com>
2023-05-21 15:32:26 -0700

    add router proto

--- milena-protos/build.rs
+++ milena-protos/build.rs
@@ -1,4 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("proto/cache_server.proto")?;
+    tonic_build::compile_protos("proto/router_server.proto")?;
     Ok(())
 }
--- milena-protos/src/lib.rs
+++ milena-protos/src/lib.rs
@@ -1,4 +1,7 @@
-use tonic::{transport::Server, Request, Response, Status};
 pub mod cache_server {
     tonic::include_proto!("cache_server");
+}
+
+pub mod router_server {
+    tonic::include_proto!("router_server");
 }
